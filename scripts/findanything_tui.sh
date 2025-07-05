0713dd03ad3b8d1fef596ed2c37e0b54bd01ce29
Daud Farzand <df89@student.london.ac.uk>
2025-07-05 22:40:29 +0500

    fixed-reinstall--safer-install

--- scripts/findanything_tui.sh
+++ scripts/findanything_tui.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+
 echo "Launching FindAnything TUI..."
 
-QUERY="$1"
-
-fd "${QUERY:-.}" / --hidden --type f --type d --follow | \
-fzf --prompt="FindAnything > " --height=40% --multi --preview="head -n 50 {}"
+fd . / \
+  --hidden \
+  --exclude ".git" \
+  --color always \
+  | fzf --preview '
+    if file --mime-type {} | grep -q text; then
+      head -n 50 {}
+    else
+      echo "[Binary file: {}]"
+    fi
+  ' --preview-window=right:60%:wrap \
+    --prompt "FindAnything > "
